3353bf2e69ba4df5a98202d453c8b15a09ea5378
Mateus da Silva Teixeira <mateusdasilvateixeira123@gmail.com>
2024-08-07 10:45:40 -0300

    a

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './App.css'
+import Tasks from './Componentes/Tasks'
+const App = () => {
+  return (
+    <>
+   <div className='container'>
+   </div>
+   </>
+  )
+}
+export default App;
