da44b9b2dbe2d738b79e397b85287ad152a13ba6
Mateus da Silva Teixeira <mateusdasilvateixeira123@gmail.com>
2024-08-07 11:12:47 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 import './App.css'
 import Tasks from './Componentes/Tasks'
+import { getByTitle } from '@testing-library/react';
+
+
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "swaws",
+      completed: false,
+    },
+    id: "2",
+    title: "sas",
+    completed: true,
+  ])
   return (
     <>
    <div className='container'>
